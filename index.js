2ee232c378582d6f2c2b3aa60489894d5eb1c902
Nagababu <nagababuchilukuri34@gmail.com>
2019-10-29 12:58:56 -0500

    added port

--- index.js
+++ index.js
@@ -70,6 +70,11 @@ app.get('/',(req,res)=>{
    res.redirect('/register');
 });
 //Listening to nodeJS Application
-app.listen(3000, function(){
-   console.log("Listening to port 3000")
+//app.listen(process.env.PORT || 5000, function(){
+  // console.log("Listening to port 3000")
+//});
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+   console.log('Listening on port %d', server_port);
 });
